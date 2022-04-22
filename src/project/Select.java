be0278832c2a29699c6ecd3830477dd801d7cdda
Suhas R <56556749+Suhasr76@users.noreply.github.com>
2022-04-22 13:19:01 +0530

    Add files via upload

---
+++ src/project/Select.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Vijay Murugan A S
+ */
+public class Select {
+    public static ResultSet getData(String query)
+    {
+        Connection con = null;
+        ResultSet rs = null;
+        Statement st = null;
+        try
+        {
+            con = ConnectionProvider.getCon();
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+            return rs;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+}
